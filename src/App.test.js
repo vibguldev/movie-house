03fbc9e21041740fb7d7114cfaae0f9cb8420c11
vibguldev <gulativibhav@gmail.com>
2017-03-02 13:36:45 +0530

    added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+import Filter from './Filter.jsx'
+const obj = new Filter
+const moviesArray = [
+  {
+    'id': '25997e12-3598-4e02-be16-c0ca94f9ee7f',
+    'actors': ['Bill Tyler', 'Derek Day', 'Ricky Hunter'],
+    'releaseDate': '1974-05-17T06:17:37.623Z',
+    'name': 'tiupu enelasezi',
+    'description': 'Ac ci zo rucmu sibefivud oljesam idgu hadraz si se mum zi lebo pet ucaos. Ge anedic woweko pol sozja beahoje bivavoc fikmewmi go zulfegli dos af dazlirit iwafad. Kamelgak di hietoak dajnupo mitozdaz kaumni jocile pak na iboosjor jisodo pi.',
+    'theme': '#03b09d',
+    'rating': 2
+  },
+  {
+    'id': '4de8dffa-3724-4d12-80cb-b37bf5b537a4', 'actors': ['Derek Day'], 'releaseDate': '1967-05-31T15:33:41.374Z', 'name': 'ebe vob', 'description': 'Gikficwi jahiwge vuihito culo haj funmun wu avi ko kusepow er zuf tuvguzhi amasali. Wukutko jonle as wupu de emvomo udoozidas mopvasbaz mavosup moswohoj duz lid vilrom vuksoibu bore kuvgo ef. Ru fonaf wufhibde junici ziifed do wo peberov dap honib made sewo kigmaf nafizoj jadpomo puc fa wu.', 'theme': '#a3e210', 'rating': 5
+  },
+  {
+    'id': '54820d41-d187-437c-9589-c4154cb47321', 'actors': ['Derek Day'], 'releaseDate': '1998-08-07T07:41:53.922Z', 'name': 'juti rubpak', 'description': 'Av mad na tecu pelkapac ga ji oc herfilor juefi il levaw. Cirotmo wezipidu lowe ro non hi rugzofir mirij bevesgor nitefa vonuub daja moomiw otocadwog leriv timdetse. Tile wa ocuhimad ki uz zaj ko ekbajin jinad jav lu hon tu.', 'theme': '#d691b9', 'rating': 4
+  },
+  {
+    'id': '591daa26-5b72-4dd3-95a8-2f1639db3a0d', 'actors': ['Derek Day', 'Nathaniel Hart'], 'releaseDate': '1971-11-12T12:24:23.889Z', 'name': 'jun duruzose', 'description': 'Kono copzohu zigpijcat vira luda koweppib fotec secte gocije hu de micuza runi mob bosbitkin cebho. Vo relfohup gemmuv toman nodes ki efo loc cufe abcovsu uvemirzi zopu fuba tejzezo deszuguna nozage uga. Vu lifetim apon kizilewu urpej inde rivuc buelni asinatruf ifbalhup tigige nooksu agupiga epu.', 'theme': '#49b464', 'rating': 3
+  },
+  {
+    'id': 'c8ffe970-76ee-44f0-a03b-f42deea51344', 'actors': ['Derek Day'], 'releaseDate': '1997-04-14T00:16:48.134Z', 'name': 'mup ze', 'description': 'Suco weh ola votipcuz vihape dule ap libmerit haak zis niwuk fatge turebe cusna maf ezuf dadralnoz efe. Garave geke wino fafufege bam fej zip zazetijur fataji hapriz casudbi wuto ti ginbup icahilhuv wevjudi. Fa oza bari ira ogkoldub fokeum motnazo jukreiko go nuz rolsenuvu kag hitzef bomob bawic fadugpi nak curuw.', 'theme': '#86d522', 'rating': 3
+  }
+]
+it('filters the movies based on Actor Name', () => {
+  const actormovies = obj.filterMovies (moviesArray)
+  console.log(Object.keys(actormovies).length)
+  expect(Object.keys(actormovies).length).toEqual(4)
+})
